0d20564a4965bb391c2af46073753d0f7625b264
SergFediaev <sergfediaev@gmail.com>
2025-04-19 11:40:45 +0300

    feat: library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react(),
+		dts({ tsconfigPath: './tsconfig.app.json' }),
+		tailwindcss(),
+	],
+	build: {
+		lib: {
+			entry: 'src/index',
+			name: 'sat-ui',
+			formats: ['es', 'cjs', 'umd'],
+			fileName: format => `sat-ui.${format}.js`,
+		},
+	},
 })
