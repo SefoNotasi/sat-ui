61e5bea6f2ca22fa43ffa8f67a77e49f572a512f
SergFediaev <sergfediaev@gmail.com>
2025-04-19 13:28:11 +0300

    feat: настройка конечной сборки

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import { peerDependencies } from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -19,6 +20,16 @@ export default defineConfig({
 			name: 'sat-ui',
 			formats: ['es', 'cjs', 'umd'],
 			fileName: format => `sat-ui.${format}.js`,
+		},
+		rollupOptions: {
+			external: [...Object.keys(peerDependencies), 'react/jsx-runtime'],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+					'react/jsx-runtime': 'jsxRuntime',
+				},
+			},
 		},
 	},
 })
