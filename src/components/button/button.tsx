1819ba812ebf56df005b39f9cf918e543b1670fb
SergFediaev <sergfediaev@gmail.com>
2025-04-19 12:57:00 +0300

    feat: экспорт стилей

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,7 +3,9 @@ import type { ComponentPropsWithoutRef } from 'react'
 export const Button = (props: ComponentPropsWithoutRef<'button'>) => {
 	return (
 		<button
-			className={'cursor-pointer rounded bg-sky-200 px-4 py-2 hover:bg-sky-300'}
+			className={
+				'cursor-pointer rounded bg-emerald-200 px-4 py-2 hover:bg-sky-300'
+			}
 			{...props}
 		/>
 	)
